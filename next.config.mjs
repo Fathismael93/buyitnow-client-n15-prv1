e98e724a9d01df9e060a150efa0233ffd7827c70
Fathismael93 <fathismael@gmail.com>
2025-04-01 18:15:01 +0300

    Checking if script-src header related error is gone

--- next.config.mjs
+++ next.config.mjs
@@ -37,7 +37,7 @@ const securityHeaders = [
   {
     key: 'Content-Security-Policy',
     value:
-      "default-src 'self'; worker-src 'self'; manifest-src 'self'; frame-src 'self'; script-src https://cdnjs.cloudflare.com; style-src 'self' https://cdnjs.cloudflare.com; media-src 'self' https://res.cloudinary.com ; img-src 'self' data: blob: https://res.cloudinary.com; font-src 'self' https://cdnjs.cloudflare.com; connect-src 'self' https://res.cloudinary.com https://sentry.io https://*.ingest.sentry.io https://*.sentry.io;",
+      "default-src 'self'; worker-src 'self'; manifest-src 'self'; frame-src 'self'; script-src 'self' 'unsafe-inline' https://cdnjs.cloudflare.com; style-src 'self' https://cdnjs.cloudflare.com; media-src 'self' https://res.cloudinary.com ; img-src 'self' data: blob: https://res.cloudinary.com; font-src 'self' https://cdnjs.cloudflare.com; connect-src 'self' https://res.cloudinary.com https://sentry.io https://*.ingest.sentry.io https://*.sentry.io;",
   },
 ];
 
