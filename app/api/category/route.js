f99f598f5f25011bfd893dae5417c5240a036bb2
Fathismael93 <fathismael@gmail.com>
2025-04-10 19:40:30 +0300

    Implemented powerful caching to api/category/route.js

--- app/api/category/route.js
+++ app/api/category/route.js
@@ -124,7 +124,6 @@ function generateCacheKey(req) {
  */
 export async function GET(req) {
   const cacheKey = generateCacheKey(req);
-  console.log('Cache Key:', cacheKey); // Debugging
   const startTime = performance.now();
 
   try {
