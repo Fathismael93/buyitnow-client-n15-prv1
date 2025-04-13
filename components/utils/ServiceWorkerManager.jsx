d19994e3c811a665652bbeabc4255b63dee458b3
Fathismael93 <fathismael@gmail.com>
2025-04-13 19:45:15 +0300

    Added ServiceWorkerManager.jsx

--- components/utils/ServiceWorkerManager.jsx
+++ components/utils/ServiceWorkerManager.jsx
@@ -19,18 +19,24 @@ const ServiceWorkerManager = () => {
     // Désactiver la mise en cache en développement
     if (!isProduction && 'serviceWorker' in navigator) {
       // Désinscrire tout Service Worker existant en mode développement
-      navigator.serviceWorker.getRegistrations().then((registrations) => {
-        for (const registration of registrations) {
-          registration.unregister();
-          console.log(
-            'Service Worker désinscrit (environnement de développement)',
+      navigator.serviceWorker
+        .getRegistrations()
+        .then((registrations) => {
+          for (const registration of registrations) {
+            registration.unregister();
+            console.log(
+              'Service Worker désinscrit (environnement de développement)',
+            );
+          }
+        })
+        .catch((error) => {
+          console.error(
+            'Erreur lors de la désinscription du Service Worker:',
+            error,
           );
-        }
-      });
+        });
     }
-  }, []).catch((error) => {
-    console.error('Erreur lors de la désinscription du Service Worker:', error);
-  });
+  }, []);
 
   // N'intégrer le script que dans l'environnement de production
   if (
