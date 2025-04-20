571fdc3278c95e5f35cbbf61cbf6011152c54481
Fathismael93 <fathismael@gmail.com>
2025-04-20 11:11:14 +0300

    Updated Login.jsx, app/login/error.jsx and app/login/page.jsx

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -132,11 +132,6 @@ async function LoginPage() {
                 <div className="absolute inset-0 flex items-center">
                   <div className="w-full border-t border-gray-300" />
                 </div>
-                <div className="relative flex justify-center text-sm">
-                  <span className="px-2 bg-white text-gray-500">
-                    Pas encore inscrit ?
-                  </span>
-                </div>
               </div>
 
               <div className="mt-6 text-center">
