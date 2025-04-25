cae1d60c46029a926777d7e3a9a0f45b798db99d
Fathismael93 <fathismael@gmail.com>
2025-04-25 23:07:25 +0300

    Updated Cart.jsx

--- components/cart/Cart.jsx
+++ components/cart/Cart.jsx
@@ -7,6 +7,7 @@ import { useRouter } from 'next/navigation';
 import ItemCart from './components/ItemCart';
 import CartContext from '@/context/CartContext';
 import { DECREASE, INCREASE } from '@/helpers/constants';
+//loading
 import Loading from '@/app/loading';
 
 const Cart = () => {
