6a6872c6973342f02d3337906388e580af093658
Fathismael93 <fathismael@gmail.com>
2025-04-25 13:44:59 +0300

    Updated setCartToState method in CartContext.js

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -4,6 +4,8 @@ import { getServerSession } from 'next-auth';
 const isAuthenticatedUser = async (req, res) => {
   const session = await getServerSession(auth);
 
+  console.log('Session in middleware', session);
+
   if (!session) {
     return res.error('Login first to access this route', 401);
   }
