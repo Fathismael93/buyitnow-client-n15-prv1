e905ff243cad6a33605102d529052703816652bc
Fathismael93 <fathismael@gmail.com>
2025-04-29 11:24:28 +0300

    Updated helpers.js

--- helpers/helpers.js
+++ helpers/helpers.js
@@ -155,3 +155,35 @@ export const capitalizeFirstLetter = (str) => {
 export const safeValue = (value, defaultValue = '') => {
   return value === undefined || value === null ? defaultValue : value;
 };
+
+export const getPriceQueryParams = (queryParams, key, value) => {
+  const hasValueInParam = queryParams.has(key);
+
+  if (value && hasValueInParam) {
+    queryParams.set(key, value);
+  } else if (value) {
+    queryParams.append(key, value);
+  } else if (hasValueInParam) {
+    queryParams.delete(key);
+  }
+  return queryParams;
+};
+
+export const getCookieName = () => {
+  let cookieName = '';
+
+  if (process.env.NODE_ENV === 'development') {
+    cookieName = 'next-auth.session-token';
+  }
+
+  if (process.env.NODE_ENV === 'production') {
+    cookieName = '__Secure-next-auth.session-token';
+  }
+
+  return cookieName;
+};
+
+export const parseCallbackUrl = (url) => {
+  const res = url.replace(/%3A/g, ':').replace(/%2F/g, '/');
+  return res;
+};
