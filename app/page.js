4ed0d245464b0421c62c3d4f8676700d72ff8cda
Fathismael93 <fathismael@gmail.com>
2025-09-03 00:35:40 +0300

    Updated getAllProducts in server-only-methods.js

--- app/page.js
+++ app/page.js
@@ -8,6 +8,8 @@ import ListProductsSkeleton from '@/components/skeletons/ListProductsSkeleton';
 // Utilisation de lazy au lieu de dynamic pour éviter le conflit de nom
 const ListProducts = lazy(() => import('@/components/products/ListProducts'));
 
+export const dynamic = 'force-dynamic'; // Force le rendu dynamique
+
 export const metadata = {
   title: 'Buy It Now - Votre boutique en ligne',
   description:
