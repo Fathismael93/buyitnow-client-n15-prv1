f7bc5288768395b6c0dcdb51403c3c730cde8413
Fathismael93 <fathismael@gmail.com>
2025-09-02 11:08:39 +0300

    Updated some apis

--- app/api/products/route.js
+++ app/api/products/route.js
@@ -1,6 +1,7 @@
 import { NextResponse } from 'next/server';
 import dbConnect from '@/backend/config/dbConnect';
 import Product from '@/backend/models/product';
+import Category from '@/backend/models/category';
 import APIFilters from '@/backend/utils/APIFilters';
 import { captureException } from '@/monitoring/sentry';
 // import {
