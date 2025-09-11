80f4fe32201353d9debce2c31701aa3d58b505b9
Fathismael93 <fathismael@gmail.com>
2025-09-11 09:08:54 +0300

    Updated icons in UserAddresses.jsx

--- components/user/UserAddresses.jsx
+++ components/user/UserAddresses.jsx
@@ -2,6 +2,7 @@ import Link from 'next/link';
 import { memo } from 'react';
 
 import { isArrayEmpty } from '@/helpers/helpers';
+import { MapPin } from 'lucide-react';
 
 const UserAddresses = memo(({ addresses }) => {
   return isArrayEmpty(addresses) ? (
@@ -15,7 +16,7 @@ const UserAddresses = memo(({ addresses }) => {
           <figure className="w-full flex align-center bg-gray-100 hover:bg-blue-100 p-4 rounded-md cursor-pointer">
             <div className="mr-3">
               <span className="flex items-center justify-center text-yellow-500 w-12 h-12 bg-white rounded-full shadow-sm mt-2">
-                <i className="fa fa-map-marker-alt"></i>
+                <MapPin />
               </span>
             </div>
             <figcaption className="text-gray-600">
